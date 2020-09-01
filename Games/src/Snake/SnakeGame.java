46e4b6272bae8991eb837a5a9373868372e813fe
phisa <phisa@localhost>
2020-09-01 12:18:05 -0400

    Final commit

---
+++ Games/src/Snake/SnakeGame.java
@@ -0,0 +1,133 @@
+package Snake;
+import java.applet.Applet;
+import java.awt.*;
+import java.awt.event.KeyEvent;
+import java.awt.event.KeyListener;
+public class SnakeGame extends Applet implements Runnable, KeyListener {
+	Graphics gfx;
+	Image img;
+	Thread thread;
+	Snake snake;
+	Token token;
+	boolean gameOver;
+	//initialize
+	public void init(){
+		//create applet size
+		this.resize(400, 400);
+		gameOver = false;
+		img = createImage(400,400);
+		gfx = img.getGraphics();
+		this.addKeyListener(this);
+		snake = new Snake();
+		token = new Token(snake);
+		thread = new Thread(this);
+		thread.start();
+	}
+	
+	public void paint(Graphics g){
+		gfx.setColor(Color.BLACK);
+		gfx.fillRect(0, 0, 400, 400);
+		if(!gameOver){
+			snake.draw(gfx); // draw the offscreen graphics
+			token.draw(gfx);
+		}
+		else{
+			gfx.setColor(Color.red);
+			gfx.drawString("Game Over", 180, 150);
+			gfx.drawString("Score: " + token.getScore(), 180, 300);
+		}
+		g.drawImage(img, 0, 0, null);
+	}
+	
+	public void update(Graphics g){
+		paint(g);
+	}
+	
+	public void repaint(Graphics g){
+		paint(g);
+	}
+
+	public void run() {
+		for(;;){
+			
+			if(!gameOver){
+				snake.move();
+				this.checkGameOver();
+				token.snakeCollision();
+			}
+			
+			this.repaint();
+			try {
+				Thread.sleep(20);
+			} catch (InterruptedException e) {
+				e.printStackTrace();
+			}
+			
+			
+		}
+		
+	}
+	public void checkGameOver(){
+		if(snake.getX() < 0 || snake.getX() > 396){
+			gameOver = true;
+		}
+		if(snake.getY() < 0 || snake.getY() > 396){
+			gameOver = true;
+		}
+		if(snake.snakeCollision()){
+			gameOver = true;
+		}
+	}
+
+	public void keyTyped(KeyEvent e) {
+		
+	}
+
+
+	public void keyPressed(KeyEvent e) {
+		if(!snake.isMoving()){
+			if(e.getKeyCode() == KeyEvent.VK_UP || e.getKeyCode() == KeyEvent.VK_DOWN || e.getKeyCode() == KeyEvent.VK_RIGHT){
+				snake.setIsMoving(true);
+			}
+		}
+
+		if(e.getKeyCode() == KeyEvent.VK_UP){
+			// if snake is not going down we will allow the snake to go up and not going side way
+			if(snake.getYDir() != 1){
+				snake.setYDir(-1);
+				snake.setXDir(0);
+				
+			}
+			
+		}
+		if(e.getKeyCode() == KeyEvent.VK_DOWN){
+			if(snake.getYDir() != -1){
+				snake.setYDir(1);
+				snake.setXDir(0);
+				
+			}
+				}
+		if(e.getKeyCode() == KeyEvent.VK_LEFT){
+			if(snake.getXDir() != 1){
+				snake.setXDir(-1);
+				snake.setYDir(0);
+				
+			}
+		}
+		if(e.getKeyCode() == KeyEvent.VK_RIGHT){
+			if(snake.getXDir() != -1){
+				snake.setXDir(1);
+				snake.setYDir(0);
+				
+			}
+		}
+		
+		
+	}
+
+	public void keyReleased(KeyEvent e) {
+	
+		
+	}
+	
+}
